0cb62da87f2f101cb0d9844f5a7087d503d9f747
“Superbiubiu” <3313899151@qq.com>
2020-07-20 10:02:12 +0800

    代理proxy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { Menu } from 'antd';
-import { HomeOutlined, UserOutlined, EyeOutlined ,
+import { HomeOutlined, UserOutlined, UnorderedListOutlined ,
     ShoppingCartOutlined} from '@ant-design/icons';
 
 import 'antd/dist/antd.css'
@@ -27,7 +27,7 @@ class App extends React.Component {
         }, {
             title: '分类',
             path: '/Sort',
-            icon: < EyeOutlined />,
+            icon: < UnorderedListOutlined />,
             component:Sort,
         }, {
             title: '购物车',
